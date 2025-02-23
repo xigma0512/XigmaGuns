feccc0b44c4fcdbabf42d49afd2df1d556ee9bfd
xig1517 <xig1517@gmail.com>
2025-02-23 13:11:22 +0800

    feat: `SmokeBomb`
    - using `SmokeBomb.create(...)` to summon a smoke

---
+++ src/source/equipment/SmokeBomb.ts
@@ -0,0 +1,23 @@
+import { system } from "@minecraft/server";
+import { Dimension } from "@minecraft/server";
+import { Vector3 } from "@minecraft/server";
+
+export class SmokeBomb {
+
+    constructor(
+        private _dimension: Dimension, 
+        private _location: Vector3,
+        private _timer: number
+    ) { }
+
+    private run(taskId: number) {
+        if (--this._timer <= 0) system.clearRun(taskId); 
+        for (let i=0; i<2; i++) this._dimension.spawnParticle('minecraft:huge_explosion_emitter', this._location);
+    }
+
+    static create(dimension: Dimension, location: Vector3, timer: number) {
+        const instance = new this(dimension, location, timer);
+        const taskId = system.runInterval(() => instance.run(taskId), 1);
+    }
+
+}
