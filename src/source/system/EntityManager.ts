dcea624cadf814519801506e1d09b673745d63b3
xig1517 <xig1517@gmail.com>
2025-02-27 23:02:39 +0800

    refactor:  為 `EntityManager` 新增方法 `getEntity` 來讀取生物或物品的 Entity 資料

--- src/source/system/EntityManager.ts
+++ src/source/system/EntityManager.ts
@@ -1,11 +1,25 @@
-import { Entity, IEntity } from "../entity/Entity";
+import { Entity } from "../entity/Entity";
 
 import { CryptoUtils } from "../../utils/Crypto";
 
+import { Entity as mcEntity } from "@minecraft/server";
+import { ItemStack } from "@minecraft/server";
+
+type EntityType = mcEntity | ItemStack;
+
 export class EntityManager {
 
-    private static _entities = new Map<string, IEntity>;
-    static getEntities() { return this._entities; }
+    private static _entities = new Map<string, Entity>;
+    
+    static getEntity(entity: EntityType) {
+        const uuid = entity.getDynamicProperty('xigmaguns:uuid') as string;
+        if (uuid === undefined) throw "[ERROR] This Entity doesn't have property: 'xigmaguns:uuid'";
+
+        const en = this._entities.get(uuid) as Entity;
+        if (en === undefined) throw `[ERROR] Cannot found entity with uuid: ${uuid}`;
+
+        return this._entities.get(uuid) as Entity;
+    }
 
     static registerEntity(entity: Entity) {
         const uuid = CryptoUtils.randomUUID();
