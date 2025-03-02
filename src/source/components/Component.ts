c40cb2ad067baa55d3a1248fac784ec46baa46c7
xig1517 <xig1517@gmail.com>
2025-03-03 03:42:49 +0800

    feat: `ParticleComponent`

--- src/source/components/Component.ts
+++ src/source/components/Component.ts
@@ -1,12 +1,14 @@
 import { HeaderComponent } from "./HeaderComponent";
 import { ItemComponent } from "./ItemComponent";
 import { MagazineComponent } from "./MagazineComponent";
+import { ParticleComponent } from "./ParticleComponent";
 import { TimerComponent } from "./TimerComponent";
 
 export declare type ComponentTypes = {
     'header': HeaderComponent;
     'item': ItemComponent;
     'magazine': MagazineComponent;
+    'particle': ParticleComponent;
     'timer': TimerComponent;
 }
 
