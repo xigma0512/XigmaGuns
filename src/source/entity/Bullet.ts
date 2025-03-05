1576a75abeee59f55fbabc09607d2e79a222b717
xig1517 <xig1517@gmail.com>
2025-03-05 14:14:56 +0800

    feat: 新增實體 `Bullet`

---
+++ src/source/entity/Bullet.ts
@@ -0,0 +1,19 @@
+import { BulletData } from "../../declare/entityData/Bullet";
+import { BulletComponent } from "../components/BulletComponent";
+import { ParticleComponent } from "../components/ParticleComponent";
+import { PositionComponent } from "../components/PositionComponent";
+import { VectorComponent } from "../components/VectorComponent";
+import { Entity } from "./Entity";
+
+export class Bullet extends Entity {
+    constructor(uuid: string) {
+        super(uuid);
+    
+        const DATA = BulletData.Bullet;
+        this.components
+            .set('bullet', new BulletComponent())
+            .set('particle', new ParticleComponent(DATA.particle))
+            .set('position', new PositionComponent(DATA.position))
+            .set('vector', new VectorComponent())
+    }
+}
