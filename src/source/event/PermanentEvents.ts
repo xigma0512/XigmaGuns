6862c7dff9494c7963edd94fc5e8017d24ff95c4
xig1517 <xig1517@gmail.com>
2025-03-06 15:01:31 +0800

    feat: 新增 PermanentEvents 來處理常駐的接聽事項

---
+++ src/source/event/PermanentEvents.ts
@@ -0,0 +1,37 @@
+import { AfterEvents, BeforeEvents } from "./Events";
+
+import { EntityManager } from "../system/EntityManager";
+import { GunFireSystem } from "../system/gun/GunFireSystem";
+import { SmokeGrenade } from "../entity/SmokeGrenade";
+import { SmokeGenerator } from "../system/SmokeGenerator";
+
+export abstract class PermanentEvents { 
+
+    static register() {
+        
+        new AfterEvents.ItemStartUseEvent(ev => {
+            if (ev.itemStack.hasTag('xigmaguns:gun')) {
+                GunFireSystem.startFire(ev.itemStack, ev.source);
+            }
+        }).subscribe();
+
+        new AfterEvents.EntitySpawnEvent(ev => {
+            if (ev.entity.typeId === 'xigmaguns:smoke_grenade') {
+                EntityManager.registerEntity(new SmokeGrenade(), ev.entity);
+            }
+        }).subscribe();
+
+        new BeforeEvents.EntityRemoveEvent(ev => {
+            const entity = EntityManager.getEntity(ev.removedEntity);
+            if (entity === undefined) return;
+
+            if (ev.removedEntity.typeId === 'xigmaguns:smoke_grenade') {
+                SmokeGenerator.create(ev.removedEntity.dimension, ev.removedEntity.location, entity);
+            }
+
+            EntityManager.unRegisterEntity(entity.uuid);
+        }).subscribe();
+
+    }
+
+}
