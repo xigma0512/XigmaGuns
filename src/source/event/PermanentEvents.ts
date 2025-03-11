8bc138989d448be700d95e26484252b4471101d6
xig1517 <xig1517@gmail.com>
2025-03-11 10:33:37 +0800

    feat: 加入 ProjectileHitEntityEvent 來偵測子彈擊中實體

--- src/source/event/PermanentEvents.ts
+++ src/source/event/PermanentEvents.ts
@@ -3,6 +3,8 @@ import { GunFireSystem } from "../system/combat/GunFireSystem";
 import { SmokeGrenade } from "../entity/SmokeGrenade";
 import { SmokeGenerator } from "../system/SmokeGenerator";
 import { GunReloadSystem } from "../system/combat/GunReloadSystem";
+import { DamageSystem } from "../system/combat/DamageSystem";
+import { BulletSystem } from "../system/combat/BulletSystem";
 
 import { system, world } from "@minecraft/server";
 import { Player } from "@minecraft/server";
@@ -24,13 +26,34 @@ export abstract class PermanentEvents {
             }
         });
 
+        world.afterEvents.projectileHitEntity.subscribe(ev => {
+            const bullet = EntityManager.getEntity(ev.projectile);
+            if (bullet === undefined) return;
+            
+            const target = ev.getEntityHit().entity!;
+            if (!(target instanceof Player)) return;
+            
+            if (bullet.hasComponent('bullet')) {
+                const owner = bullet.getComponent('bullet')!.owner!;
+                const hitType = BulletSystem.getHitType(ev.location, target);
+                new DamageSystem(owner, target).applyGunDamage(bullet, hitType);
+            }
+
+            BulletSystem.launchLocus(ev.projectile, ev.location);
+        });
+
+        world.afterEvents.playerSpawn.subscribe(ev => {
+            if (!ev.initialSpawn) return;
+            ev.player.setDynamicProperty('xigmaguns:team', 0);
+        });
+
         system.afterEvents.scriptEventReceive.subscribe(ev => {
             const player = ev.sourceEntity as Player;
             if (ev.id === 'xigmaguns:reload') {
                 const item = player.getComponent('equippable')?.getEquipmentSlot(EquipmentSlot.Mainhand).getItem();
                 if (item === undefined) return;
                 GunReloadSystem.create(player, item);
-            } 
+            }
         });
 
         world.beforeEvents.entityRemove.subscribe(ev => {
