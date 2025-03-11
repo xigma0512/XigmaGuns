809e4de9d223eb08b8f88f9cedefe136868a0ad0
xig1517 <xig1517@gmail.com>
2025-03-11 11:15:33 +0800

    refactor: 修改 `GunFireSystem` 架構

--- src/source/system/combat/GunFireSystem.ts
+++ src/source/system/combat/GunFireSystem.ts
@@ -1,61 +1,75 @@
 import { Entity } from "../../entity/Entity";
-import { MagazineComponent } from "../../components/MagazineComponent";
 import { BulletSystem } from "./BulletSystem";
-import { GunComponent } from "../../components/GunComponent";
-
 import { IntervalTask, TaskManager } from "../TaskManager";
 import { EntityManager } from "../EntityManager";
 
+import { MagazineComponent } from "../../components/MagazineComponent";
+import { GunComponent } from "../../components/GunComponent";
+
 import { world } from "@minecraft/server";
+import { EntityDieAfterEvent, ItemStopUseAfterEvent } from "@minecraft/server";
 import { ItemStack, Player } from "@minecraft/server";
 
 export class GunFireSystem {
 
-    static startFire(item: ItemStack, owner: Player) {
+    private readonly _owner: Player;
+    private readonly _entity: Entity;
 
-        const entity = EntityManager.getEntity(item) as Entity;
-        if (entity === undefined) return false;
+    private _taskId = -1;
 
-        const gunComp = entity.getComponent('gun') as GunComponent;
+    private _itemStopUseListener?: EventType<ItemStopUseAfterEvent>;
+    private _playerDieListener?: EventType<EntityDieAfterEvent>;
 
-        const task = new IntervalTask({
+    private constructor(owner: Player, weaponItem: ItemStack) {
+        this._owner = owner;
+        this._entity = EntityManager.getEntity(weaponItem) as Entity;
+        if (this._entity === undefined) throw '[ERROR] 找不到實體資料';
+
+        this.beginning();
+    }
+
+    private beginning() {
+        const gunComp = this._entity.getComponent('gun') as GunComponent;
+
+        const consumeAmmo = () => {
+            const magazineComp = this._entity.getComponent('magazine') as MagazineComponent;
+            if (magazineComp.ammo === 0) return false;
+            magazineComp.ammo--;
+            return true;
+        }
+
+        this._taskId = TaskManager.executeTask(new IntervalTask({
             duration: 3600,
             interval: gunComp.fireRate,
-            tickFunction() {
-                if (GunFireSystem.consumeAmmo(entity)) {
-                    return BulletSystem.summonBullet(owner, gunComp);
+            tickFunction: () => {
+                if (consumeAmmo()) {
+                    return BulletSystem.summonBullet(this._owner, gunComp);
                 }
-                owner.onScreenDisplay.setActionBar('YOU HAVE NO AMMO.');
+                this._owner.onScreenDisplay.setActionBar('YOU HAVE NO AMMO.');
             }
+        }));
+        this.watchInterruptions();
+    }
+
+    private watchInterruptions() {
+        this._itemStopUseListener = world.afterEvents.itemStopUse.subscribe(ev => {
+            if (ev.source.id === this._owner.id) this.interruption();
         });
-        this.fireInterruption(TaskManager.executeTask(task), owner.name);
-
-        return true;
-    }
     
-    private static fireInterruption(taskId: number, ownerName: string) {
-        const [stopUseItem, playerDie] = [
-            world.afterEvents.itemStopUse.subscribe(ev => {
-                if (ev.source.name === ownerName) stopFire();
-            }),
-            world.afterEvents.entityDie.subscribe(ev => {
-                if (!(ev.deadEntity instanceof Player)) return;
-                if (ev.deadEntity.name === ownerName) stopFire();
-            })
-        ];
-
-        const stopFire = () => {
-            TaskManager.removeTask(taskId);
-            world.afterEvents.itemStopUse.unsubscribe(stopUseItem);
-            world.afterEvents.entityDie.subscribe(playerDie);
-        }
+        this._playerDieListener = world.afterEvents.entityDie.subscribe(ev => {
+            if (!(ev.deadEntity instanceof Player)) return;
+            if (ev.deadEntity.id === this._owner.id) this.interruption();
+        });
     }
 
-    private static consumeAmmo(entity: Entity) {
-        const magazineComp = entity.getComponent('magazine') as MagazineComponent;
-        if (magazineComp.ammo === 0) return false;
-        magazineComp.ammo--;
-        return true;
+    private interruption() {
+        TaskManager.removeTask(this._taskId);
+        world.afterEvents.itemStopUse.unsubscribe(this._itemStopUseListener!);
+        world.afterEvents.entityDie.unsubscribe(this._playerDieListener!);
+    }
+
+    static create(owner: Player, weaponItem: ItemStack) {
+        new this(owner, weaponItem);
     }
 
 }
